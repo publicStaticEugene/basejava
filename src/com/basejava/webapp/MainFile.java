60d07100a30b901dc308c6eaaab65677dfb52850
publicStaticEugene <eugene.y72@gmail.com>
2018-03-26 20:25:00 +0300

    HW8(update MainFile)

--- src/com/basejava/webapp/MainFile.java
+++ src/com/basejava/webapp/MainFile.java
@@ -1,15 +1,30 @@
 package com.basejava.webapp;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.Objects;
 
 public class MainFile {
-    public static void main(String[] args) throws IOException {
-        File file = new File(".\\.gitignore");
-        System.out.println(String.format("Absolute path  === %s", file.getAbsolutePath()));
-        System.out.println(String.format("Canonical path === %s", file.getCanonicalPath()));
-        System.out.println(String.format("Path           === %s", file.getPath()));
-        System.out.println(String.format("Name           === %s", file.getName()));
-        System.out.println(String.format("Parent         === %s", file.getParent()));
+    public static void main(String[] args) {
+        File dir = new File(".");
+//        System.out.println(String.format("Absolute path  === %s", file.getAbsolutePath()));
+//        System.out.println(String.format("Canonical path === %s", file.getCanonicalPath()));
+//        System.out.println(String.format("Path           === %s", file.getPath()));
+//        System.out.println(String.format("Name           === %s", file.getName()));
+//        System.out.println(String.format("Parent         === %s", file.getParent()));
+        printFiles(dir);
+    }
+
+    public static void printFiles(File dir) {
+        Objects.requireNonNull(dir);
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException(dir + "is not directory");
+        }
+        File[] list = dir.listFiles();
+        for (File entity : list) {
+            if (entity.isDirectory()) {
+                printFiles(entity);
+            }
+            System.out.println(entity);
+        }
     }
 }
